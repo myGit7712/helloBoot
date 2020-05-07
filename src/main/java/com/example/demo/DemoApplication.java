a546a74f1937cda4247fc22060b475023cafc56e
范生 <5459027+fan_sheng_cloud@user.noreply.gitee.com>
2020-05-07 15:41:04 +0800

    12345

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("123");
+        System.out.println("123456");
     }
 
 }
