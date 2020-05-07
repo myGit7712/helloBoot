41ff96c13105a4d9b9d7aad23645ed2ace8d5d63
范生 <5459027+fan_sheng_cloud@user.noreply.gitee.com>
2020-05-07 15:01:03 +0800

    分支2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 public class DemoApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
